b82da43c48f49d34fe7b403c6df53e02f729d4c0
IbrahimAjmal02 <Ibrahimajmal02@gmail.com>
2025-02-19 19:04:05 -0500

    Cart update

---
+++ app/cart/page.js
@@ -0,0 +1,16 @@
+"use client"
+
+import MainLayout from "../layouts/MainLayout"
+import SimilarProducts from "../components/SimilarProducts"
+
+export default function Cart() {
+    return (
+        <>
+            <MainLayout>
+
+
+                <SimilarProducts />
+            </MainLayout>
+        </>
+    )
+}
