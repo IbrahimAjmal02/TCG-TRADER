f4e2195263e3ce149a280ce4c6c1886a72c76aa5
IbrahimAjmal02 <Ibrahimajmal02@gmail.com>
2025-02-20 21:15:30 -0500

    Order In progress

---
+++ app/orders/page.js
@@ -0,0 +1,14 @@
+"use client"
+
+import MainLayout from "../layouts/MainLayout"
+
+
+export default function TopMenu() {
+    return (
+        <>
+            <MainLayout>
+                
+            </MainLayout>
+        </>
+    )
+}
