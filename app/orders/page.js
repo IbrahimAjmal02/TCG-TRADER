431e595467aff9f73be6bb6ff9716f0841fb31e2
IbrahimAjmal02 <Ibrahimajmal02@gmail.com>
2025-02-22 00:40:51 -0500

    Orders finished!

--- app/orders/page.js
+++ app/orders/page.js
@@ -5,11 +5,79 @@ import { CiDeliveryTruck } from 'react-icons/ci'
 import MainLayout from "../layouts/MainLayout"
 
 
-export default function TopMenu() {
+export default function Orders() {
+    const orders = [
+        {
+            id: 1,
+            stripe_id: "123123123",
+            name: "Doffy",
+            address: "Dressrosa",
+            zipcode: "0N3P13C3",
+            city: "Dressrosa Country",
+            country: "Dressrosa City",
+            total: 36718,
+            orderItem: [
+                {
+                id: 1,
+                title: "Pokemon Charizard - 4/102 - Holo Rare Collectible Card ",
+                url: "https://static1.cbrimages.com/wordpress/wp-content/uploads/2023/02/charizard-1st-edition-from-pokemon-tcg.jpg?q=70&fit=crop&w=750&dpr=1",
+                }
+            ]
+        }
+    ]
     return (
         <>
             <MainLayout>
-                
+                <div id="OrdersPage" className="mt-4 max-w-[1200px] mx-auto px-2 min-h-[50vh]">
+                    <div className="bg-white w-full p-6 min-h-[150px]">
+                        <div className="flex items-center text-xl">
+                            <CiDeliveryTruck className="text-green-500" size={35}/>
+                            <span className="pl-4">Orders</span>
+                        </div>
+                            {orders.length < 1 ?
+                                <div className="flex items-center justify-center">
+                                    You have no order history
+                                </div>
+                                :
+                                null                        
+                            }
+
+                            {orders.map(order => [
+                                <div key={order?.id} className="text-sm pl-[50px]">
+                                    <div className="border-b py-1">
+
+                                        <div className="pt-2">
+                                            <span className="font-bold mr-2">Stripe ID:</span>
+                                            {order?.stripe_id}
+                                        </div>
+
+                                        <div className="pt-2">
+                                            <span className="font-bold mr-2">Delivery Address:</span>
+                                            {order?.name}, {order?.address}, {order?.zipcode}, {order?.city}, {order?.country}
+                                        </div>
+
+                                        <div className="pt-2">
+                                            <span className="font-bold mr-2">Total:</span>
+                                            <span className="font-semibold">{(order?.total /100).toFixed(2)}</span>
+                                        </div> 
+
+                                        <div className="flex items-center gap-4">
+                                            {order?.orderItem.map(
+                                                item => (
+                                                    <div key={item.id} className="flex items-center">
+                                                        <Link href="/" className="py-1 hover:underline text-blue-500 font-bold">
+                                                            <img className="rounded" width="120" src={item.url+'/120'}/>
+                                                            {item.title}
+                                                        </Link>
+                                                    </div>
+                                                )
+                                            )}
+                                        </div>                                  
+                                    </div>
+                                </div>
+                            ])}
+                    </div>
+                </div> 
             </MainLayout>
         </>
     )
